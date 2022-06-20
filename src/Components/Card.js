ee4a2939a0d84e3f65fa6f22dad7c16004815f24
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-19 21:38:23 -0700

    mycard

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,148 +4,154 @@ import "./MyCard.css";
 const Card = () => {
   return (
     <>
-    <section className="container">
-      <MyCard
-        title="Armaani"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://media.istockphoto.com/photos/african-american-woman-in-jacket-with-hands-in-pockets-looking-at-picture-id1207472153?k=20&m=1207472153&s=612x612&w=0&h=aYoMBd2aQoowzUGpLqEKQIS1AnI6I6Mx1qXWGs8smXM="
-        }
-      />
-      <MyCard
-        title="SuperDry"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1617922001439-4a2e6562f328?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-        }
-      />
-      <MyCard
-        title="Tommy"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1618244972963-dbee1a7edc95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Gucci"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1552874869-5c39ec9288dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Monte Carlo"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Performax"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1618333452884-5c8d211ed2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Vogue"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1621184455862-c163dfb30e0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Puma"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1598554793905-075f7b355cd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Velocity"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1602303894456-398ce544d90b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Teamspirit"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1553845911-9e88516bdcbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Sega"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1617059322001-a61ce9551e08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Exchange"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1584998316204-3b1e3b1895ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Carton London"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1616313253719-c46514cddee1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Quiero"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1608912215571-61b7d5914b35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Netplay"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1500336624523-d727130c3328?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Dolce Gabba"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1551048632-24e444b48a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Peter England"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1617059153946-97005c8b5dbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Safari"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1618374645957-04e5cc73ec21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Pantalooms"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1618374889392-7bd8f8aa0dde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <MyCard
-        title="Disney"
-        name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
-        image={
-          "https://images.unsplash.com/photo-1603570394859-587376e50690?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
+      <div className="navbar">
+        <h1 className="nav-item">FashStore</h1>
+      </div>
+      <section className="container">
+        <MyCard
+          title="Armaani"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://media.istockphoto.com/photos/african-american-woman-in-jacket-with-hands-in-pockets-looking-at-picture-id1207472153?k=20&m=1207472153&s=612x612&w=0&h=aYoMBd2aQoowzUGpLqEKQIS1AnI6I6Mx1qXWGs8smXM="
+          }
+        />
+        <MyCard
+          title="SuperDry"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1617922001439-4a2e6562f328?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+          }
+        />
+        <MyCard
+          title="Tommy"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1618244972963-dbee1a7edc95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Gucci"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1552874869-5c39ec9288dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Monte Carlo"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Performax"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1618333452884-5c8d211ed2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Vogue"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1621184455862-c163dfb30e0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Puma"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1598554793905-075f7b355cd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Velocity"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1602303894456-398ce544d90b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Teamspirit"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1553845911-9e88516bdcbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Sega"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1617059322001-a61ce9551e08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Exchange"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1584998316204-3b1e3b1895ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Carton London"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1616313253719-c46514cddee1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Quiero"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1608912215571-61b7d5914b35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Netplay"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1500336624523-d727130c3328?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Dolce Gabba"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1551048632-24e444b48a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Peter England"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1617059153946-97005c8b5dbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Safari"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1618374645957-04e5cc73ec21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Pantalooms"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1618374889392-7bd8f8aa0dde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
+        <MyCard
+          title="Disney"
+          name="Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. Furthermore, Fashion is an industry."
+          image={
+            "https://images.unsplash.com/photo-1603570394859-587376e50690?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjF8fHdvbWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          }
+        />
       </section>
+      <div className="footer">
+        <h3 className="foot-item">@FashStore</h3>
+      </div>
     </>
   );
 };
